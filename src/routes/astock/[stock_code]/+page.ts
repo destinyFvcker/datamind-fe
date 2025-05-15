091de84bd8161e11b46b2b19cceb54ea5a83a2ff
destinyFvcker <destinyfunk@163.com>
2025-05-15 20:24:58 +0800

    fix: actix-web return mime type errorx5

--- src/routes/astock/[stock_code]/+page.ts
+++ src/routes/astock/[stock_code]/+page.ts
@@ -1,12 +1,12 @@
 import type { PageLoad } from './$types.js';
 
-export const entries = () => {
-	return [
-		{
-			stock_code: '0'
-		}
-	];
-};
+// export const entries = () => {
+// 	return [
+// 		{
+// 			stock_code: '0'
+// 		}
+// 	];
+// };
 
 export const load: PageLoad = ({ params }) => {
 	const { stock_code } = params;
